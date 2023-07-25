8ae0237409548b2e7dd84849fb9a64358c9e37bf
yu-teacher <yusm1231@gmail.com>
2023-07-26 00:10:44 +0900

    최초커밋

---
+++ src/programmers/level_2/triangular_snail/Solution.java
@@ -0,0 +1,37 @@
+package programmers.level_2.triangular_snail;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n*(n+1))/2];
+        int[][] matrix = new int[n][n];
+
+        int x = -1, y = 0;
+        int num = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (i % 3 == 0) {
+                    x++;
+                } else if (i % 3 == 1) {
+                    y++;
+                } else if (i % 3 == 2) {
+                    x--;
+                    y--;
+                }
+                // System.out.println("X = "+x+" Y = "+y);
+                matrix[x][y] = num++;
+            }
+        }
+
+        int k = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) break;
+                answer[k++] = matrix[i][j];
+            }
+        }
+
+        return answer;
+    }
+}
